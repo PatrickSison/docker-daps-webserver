816863b819c921a82be06379a1d76e8494dc67e8
PatrickSison <pmsison@hotmail.com>
2018-07-25 09:39:06 -0400

    Updated script to install requirements

--- start.sh
+++ start.sh
@@ -4,13 +4,16 @@ set -e
 if [ "$TESTING_ENV" = "development" ]
 then
     echo "development"
+    cd /app/api
+    pip install -r requirements.txt
     python3 manage.py makemigrations
     python3 manage.py migrate
-    python3 manage.py runserver 0.0.0.0:8080
+    python3 manage.py runserver 0.0.0.0:8080 & (cd /app/web && react-scripts start)
 else
     echo $TESTING_ENV
     cd /app/api
+    pip install -r requirements.txt
     python3 manage.py makemigrations
     python3 manage.py migrate
-    (python3 manage.py runserver 0.0.0.0:443 & sleep 10 && pip install -r requirements.txt && pip uninstall --yes celery  && cd celery-master/ && python setup.py install && cd ..&& celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
+    (python3 manage.py runserver 0.0.0.0:443 & sleep 10 && pip uninstall --yes celery  && cd celery-master/ && python setup.py install && cd ..&& celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 fi
