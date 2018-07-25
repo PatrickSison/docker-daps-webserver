66474d46bc15a90a98aba2b6b500db20e02965ca
PatrickSison <pmsison@hotmail.com>
2018-07-25 11:30:22 -0400

    Consolidated celery

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ then
     echo "development"
     cd /app/api
     pip install -r requirements.txt
+    pip uninstall --yes celery
+    cd celery-master/
+    python setup.py install 
+    cd ..
     python3 manage.py makemigrations
     python3 manage.py migrate
     python3 manage.py runserver 0.0.0.0:8080 & (cd /app/web && react-scripts start)
@@ -13,7 +17,11 @@ else
     echo $TESTING_ENV
     cd /app/api
     pip install -r requirements.txt
+    pip uninstall --yes celery
+    cd celery-master/
+    python setup.py install 
+    cd ..
     python3 manage.py makemigrations
     python3 manage.py migrate
-    (python3 manage.py runserver 0.0.0.0:443 & sleep 10 && pip uninstall --yes celery  && cd celery-master/ && python setup.py install && cd ..&& celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
+    (python3 manage.py runserver 0.0.0.0:443 & sleep 10 && celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 fi
