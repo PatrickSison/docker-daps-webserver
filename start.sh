dd51026ef082a67cd5ddd1288eff62a0f30be91c
PatrickSison <pmsison@hotmail.com>
2018-07-25 09:04:50 -0400

    Updated script to include celery

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ then
     python3 manage.py runserver 0.0.0.0:8080
 else
     echo $TESTING_ENV
+    cd /app/api
     python3 manage.py makemigrations
     python3 manage.py migrate
-    python3 manage.py runserver 0.0.0.0:443
+    (python3 manage.py runserver 0.0.0.0:443 & sleep 10 && pip install -r requirements.txt && pip uninstall --yes celery  && cd celery-master/ && python setup.py install && cd ..&& celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 fi
