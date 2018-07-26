c805ca180e9d111ee9b90053983d8d8a2c966a63
PatrickSison <pmsison@hotmail.com>
2018-07-26 12:05:45 -0400

    Updated directory

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$TESTING_ENV" = "development" ]
 then
     echo "development"
-    cd /app/api
+    cd /app
     pip3 install -r requirements.txt
     pip3 uninstall --yes celery
     cd celery-master/
@@ -12,10 +12,10 @@ then
     cd ..
     python3 manage.py makemigrations
     python3 manage.py migrate
-    python3 manage.py runserver 0.0.0.0:8080 & (cd /app/web && react-scripts start)
+    (python3 manage.py runserver 0.0.0.0:8080 & sleep 10 && celery -A api.celery worker --beat  -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler)
 else
     echo $TESTING_ENV
-    cd /app/api
+    cd /app
     pip3 install -r requirements.txt
     pip3 uninstall --yes celery
     cd celery-master/
